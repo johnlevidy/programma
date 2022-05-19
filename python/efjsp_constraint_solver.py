75ddbf6684e6987b4e3de89c7a7db2e46dcf7ba7
johnlevidy <john.levidy@gmail.com>
2022-05-18 21:02:07 -0400

    valid move getter, basic tabu search and random search

--- python/efjsp_constraint_solver.py
+++ python/efjsp_constraint_solver.py
@@ -54,10 +54,64 @@ def generate_random_solution(task_graph, operations, employees):
 
     return assignments
 
+# Adds a pointer to the assignment to the graph
 def add_assignments_to_graph(task_graph, assignments):
     for employee, tasks in assignments.items():
         for i, task in enumerate(tasks):
             task_graph.nodes[task]['assignment_pointer'] = (employee, i)
+
+# Gets all transitive deps for node passed in
+def get_transitive_dependencies(task_graph, node):
+    successors = set(task_graph.successors(node))
+    if not any(successors):
+        return set()
+    else:
+        ret = successors
+        for s in successors:
+            ret = ret.union(s)
+            ret = ret.union(get_transitive_dependencies(task_graph, s))
+        return ret 
+
+# Annotates each task in the task graph with a set of all 
+# of its transitive dependencies
+def add_transitive_dependencies_to_graph(task_graph):
+    for n, data in task_graph.nodes(data=True):
+        data['transitive_dependencies'] = get_transitive_dependencies(task_graph, n)
+
+# the high level idea here is: 
+# 1) n <- randomly selected node to move
+# 2) t <- all transitive children of n
+# then, for each P_x ( person x's task list ):
+# 1) m <- intersection(t, P_x) 
+# 2) iterate P_x, remove tasks from m as you visit, when m is empty the 
+# positions iterated over are valid
+# In plain english: for each person, i cannot work on the moved task until i've already completed any
+# of its transitive dependencies ( otherwise there's a deadlock )
+def get_valid_move(task_graph, assignments):
+    # first, choose a node at random
+    node_to_move, data = random.choice(list(task_graph.nodes(data=True)))
+    print("Moving node: " + node_to_move)
+    deps = set(data['transitive_dependencies'])
+    print(f"Deps: {deps}")
+    valid_moves = [] # List of ('Name': position<int>) pairs
+    for employee, tasks in assignments.items():
+        tmp = deps.intersection(tasks)
+        for i, task in enumerate(tasks):
+            if not any(tmp):
+                valid_moves.append((employee, i)) 
+            tmp.discard(task)
+    print(valid_moves)
+
+def tabu_search(task_graph, operations, employees, last_task):
+    best_completion_time = None
+    for i in range(0, 1000):
+        assignments = generate_random_solution(task_graph, operations, employees)
+        add_assignments_to_graph(task_graph, assignments)
+        newtime = completion_time(task_graph, assignments, last_task)
+        if not best_completion_time or newtime < best_completion_time:
+            best_completion_time = newtime
+            best_assignment = assignments
+    return best_assignment, best_completion_time
 
 def main(args):
     f = open(args.filepath)
@@ -74,26 +128,19 @@ def main(args):
         for dep in operation.deps:
             task_graph.add_edge(operation.name, dep)
 
+    add_transitive_dependencies_to_graph(task_graph)
+
     # Find the last task
     last_task = None
     for operation in operations:
         if not any(task_graph.predecessors(operation.name)):
             last_task = operation.name
 
-    sttime = time.time()
-    best_completion_time = None
-    for i in range(0, 100):
-        assignments = generate_random_solution(task_graph, operations, employees)
-        add_assignments_to_graph(task_graph, assignments)
-        newtime = completion_time(task_graph, assignments, last_task)
-        if not best_completion_time or newtime < best_completion_time:
-            best_completion_time = newtime
-            best_assignment = assignments
-    endtime = time.time()
-    print(f"Spent: ({endtime - sttime})")
+    best_assignment, best_completion_time = tabu_search(task_graph, operations, employees, last_task)
     print("Best assignment:")
     print(best_assignment)
     print(best_completion_time)
+    get_valid_move(task_graph, best_assignment)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process input operations file")
